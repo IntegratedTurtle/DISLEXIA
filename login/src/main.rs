77f61ccbdac69f04a269b9a67832c8789b22a4e1
heimchen <ben.kinigadner@gmail.com>
2025-01-07 19:13:57 +0100

    Added Guest login funktionalitaet with session storage

--- login/src/main.rs
+++ login/src/main.rs
@@ -1,16 +1,38 @@
+use leptos::wasm_bindgen::JsCast;
 use leptos::*;
+use leptos_dom::logging::{console_error, console_log};
+use wasm_bindgen::{convert::FromWasmAbi, JsValue};
+use wasm_bindgen_futures::{spawn_local, JsFuture};
+use web_sys::{console, window, RequestInit, Response, Window};
 
 fn main() {
     console_error_panic_hook::set_once();
     mount_to_body(|| view! {<App/>})
 }
 
+fn get_storage(window: &Window, field: &str) -> Option<String> {
+    if let Some(storage) = window.local_storage().ok().flatten() {
+        if let Ok(Some(data)) = storage.get_item(field) {
+            return Some(data);
+        }
+    }
+    return None;
+}
+
+fn set_storage(window: &Window, field_name: &str, data: &str) {
+    if let Some(storage) = window.local_storage().ok().flatten() {
+        if let Err(e) = storage.set_item(field_name, data) {
+            console_error(&format!("Failed to set localStorage: {:?}", e));
+        }
+    } else {
+        console_error(&"localStorage is not available.");
+    }
+}
 #[component]
 fn App() -> impl IntoView {
     let (count, set_count) = create_signal(0);
 
     view! {
-    <TypeSelection/>
     <GuestLogin/>
     <div class="container">
         <h2 class="titles">DISLEXIA</h2>
@@ -25,8 +47,6 @@ fn App() -> impl IntoView {
 #[component]
 fn LoginForm() -> impl IntoView {
     view! {
-
-        Client-side rendering (CSR) with Trunk - a great option if you just want to make a snappy website with Leptos, or work with a pre-existing server or API. In CSR mode, Trunk compiles your Leptos app to WebAssembly (WASM) and runs it in the browser like a typical Javascript single-page app (SPA). The advantages of Leptos CSR include faster build times and a quicker iterative development cycle, as well as a simpler mental model and more options for deploying your app. CSR apps do come with some disadvantages: initial load times for your end users are slower compared to a server-side rendering approach, and the usual SEO challenges that come along with using a JS single-page app model apply to Leptos CSR apps as well. Also note that, under the hood, an auto-generated snippet of JS is used to load the Leptos WASM bundle, so JS must be enabled on the client device for your CSR app to display properly. As with a
         <form action="#" class="form" method="POST">
             <input type="email" class="input" placeholder="user@example.com" required/>
             <input type="password" class="input" placeholder="Password1234" required/>
@@ -37,8 +57,60 @@ fn LoginForm() -> impl IntoView {
 
 #[component]
 fn GuestLogin() -> impl IntoView {
+    if let Some(window) = window() {
+        console_log(&format!(
+            "Guest from localStorage: {:?}",
+            get_storage(&window, "guest")
+        ));
+        console_log(&format!(
+            "Data from localStorage: {:?}",
+            get_storage(&window, "data")
+        ));
+    }
+
+    let make_request = move || {
+        spawn_local(async move {
+            let request = RequestInit::new();
+            request.set_method("SEARCH"); // Use POST for sending a body
+            request.set_body(&JsValue::from_str(
+                "{\"email\": \"user@example.com\",\"password\": \"securepassword\"}",
+            ));
+
+            if let Some(window) = window() {
+                let fetch_promise =
+                    window.fetch_with_str_and_init("http://127.0.0.1:8081", &request);
+
+                match JsFuture::from(fetch_promise).await {
+                    Ok(response_value) => {
+                        let response: web_sys::Response = response_value.dyn_into().unwrap();
+
+                        match JsFuture::from(response.text().unwrap()).await {
+                            Ok(text) => {
+                                let response_text: String = text.as_string().unwrap();
+                                set_storage(&window, "guest", "true");
+                                set_storage(&window, "data", &response_text);
+                            }
+                            Err(err) => unsafe {
+                                console::log_1(
+                                    &format!("Error reading response body: {:?}", err).into(),
+                                );
+                            },
+                        }
+                    }
+                    Err(err) => unsafe {
+                        console::log_1(&format!("Error fetching data: {:?}", err).into());
+                    },
+                }
+            }
+        });
+    };
+
     view! {
-    <button class="guest-login">Enter as Guest</button>
+        <button
+            on:click= move |_| make_request()
+            class="guest-login">
+            "Enter as Guest"
+        </button>
     }
 }
 
