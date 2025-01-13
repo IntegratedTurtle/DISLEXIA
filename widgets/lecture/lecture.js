2a3063c72043788c043c143cd204673e9f993d37
julian <146991973+IntegratedTurtle@users.noreply.github.com>
2025-01-13 16:18:12 +0100

    Progrss bar

--- widgets/lecture/lecture.js
+++ widgets/lecture/lecture.js
@@ -1,11 +1,10 @@
-function setProgressBar(value) {
-    const progressBar = document.getElementById('progress-bar');
-    progressBar.style.height = `${value}%`;
-}
 
-// Example usage: set the progress bar to 50%
-setProgressBar(50);
 
 function send_to_homescreen() {
     document.location.href = "../homescreen/homescreen.html";
+}
+
+function setProgress(percentage) {
+    const progressElement = document.querySelector('.progress');
+    progressElement.style.height = `${percentage}%`;
 }
